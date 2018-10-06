63ce770f77627dd9876ec8586f09b5b127fb26c3
Heini Ahokas <ahokas.heini@gmail.com>
2018-10-06 20:00:39 +0200

    add date conversion to contract creation date

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,53 +1,59 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemText from '@material-ui/core/ListItemText'
-import Divider from '@material-ui/core/Divider'
-import Typography from '@material-ui/core/Typography'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import axios from "axios";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+import Typography from "@material-ui/core/Typography";
+import { Link } from "react-router-dom";
 
 class ProposalList extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-  }
-  componentDidMount() {
-    axios.get('http://localhost:3001/proposals').then(({ data }) => {
-      this.setState({ data })
-      console.log(data)
-    })
-  }
-  render() {
-    return (
-      <List>
-        <Typography variant="headline">Your Contracts</Typography>
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    componentDidMount() {
+        axios.get("http://localhost:3001/proposals").then(({ data }) => {
+            this.setState({ data });
+            console.log(data);
+        });
+    }
+    render() {
+        return (
+            <List>
+                <Typography variant="headline">Your Contracts</Typography>
 
-        {this.state.data &&
-          this.state.data.map(contract => {
-            console.log(contract.id)
-            return (
-              <Link
-                to={`/proposals/${contract.id}`}
-                style={{
-                  textDecoration: 'none'
-                }}
-              >
-                <div key={contract.id}>
-                  <ListItem button>
-                    <ListItemText
-                      primary={contract.contractName || 'Contract Name'}
-                      secondary={contract.creationDate}
-                    />
-                  </ListItem>
-                  <Divider />
-                </div>
-              </Link>
-            )
-          })}
-      </List>
-    )
-  }
+                {this.state.data &&
+                    this.state.data.map(contract => {
+                        var dateNumber = parseInt(contract.creationDate);
+                        return (
+                            <Link
+                                to={`/proposals/${contract.id}`}
+                                key={contract.id}
+                                style={{
+                                    textDecoration: "none"
+                                }}
+                            >
+                                <div>
+                                    <ListItem button>
+                                        <ListItemText
+                                            primary={
+                                                contract.contractName ||
+                                                "Contract Name"
+                                            }
+                                            secondary={new Date(
+                                                dateNumber
+                                            ).toLocaleDateString()}
+                                        />
+                                    </ListItem>
+                                    <Divider />
+                                </div>
+                            </Link>
+                        );
+                    })}
+            </List>
+        );
+    }
 }
 
-export default ProposalList
+export default ProposalList;
