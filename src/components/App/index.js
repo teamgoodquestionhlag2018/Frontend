4d1748ac7f18061d65d4032177d9c5f76fae3fd1
imeugenia <jevgenijazigisova@gmail.com>
2018-10-06 13:54:04 +0200

    Setup routes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Form from '../Form'
+import List from '../List'
 
 class App extends Component {
   render() {
-    return <div>Freelancer platform</div>
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={List} />
+          <Route path="/form" component={Form} />
+        </div>
+      </Router>
+    )
   }
 }
 
