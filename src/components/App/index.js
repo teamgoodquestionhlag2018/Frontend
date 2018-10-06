1e266f3a3a255c388bef731ae7576126b2f51e04
Heini Ahokas <ahokas.heini@gmail.com>
2018-10-06 15:31:02 +0200

    add basics to list component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Form from "../Form";
+<<<<<<< HEAD
 import List from "../List";
 import Proposal from "../Proposal";
 
@@ -16,6 +17,21 @@ class App extends Component {
       </Router>
     );
   }
+=======
+import ProposalList from "../List";
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <Route exact path="/" component={ProposalList} />
+                    <Route path="/form" component={Form} />
+                </div>
+            </Router>
+        );
+    }
+>>>>>>> add basics to list component
 }
 
 export default App;
