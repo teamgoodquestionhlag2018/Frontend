317bf34b919c263f14ace19a5a024f83688ac0c2
Heini Ahokas <ahokas.heini@gmail.com>
2018-10-06 17:01:06 +0200

    fix merge conflict

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,18 +1,47 @@
 import React, { Component } from "react";
+import axios from "axios";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
 import Typography from "@material-ui/core/Typography";
 
 class ProposalList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    componentDidMount() {
+        axios.get("http://localhost:3001/proposals").then(({ data }) => {
+            this.setState({ data });
+        });
+    }
+    //{this.props.pending && this.props.pending.length ? (
+    //    this.props.pending.map(pending => {
+
     render() {
         return (
             <List>
                 <Typography variant="headline">Your Contracts</Typography>
-                <ListItem button>
-                    <ListItemText primary="Proposal Name" />
-                </ListItem>
+
+                {this.state.data &&
+                    this.state.data.map(contract => {
+                        return (
+                            <div key={contract.id}>
+                                <ListItem button>
+                                    <ListItemText
+                                        primary={
+                                            contract.contractName ||
+                                            "Contract Name"
+                                        }
+                                        secondary={contract.creationDate}
+                                    />
+                                </ListItem>
+                                <Divider />
+                            </div>
+                        );
+                    })}
             </List>
         );
     }
