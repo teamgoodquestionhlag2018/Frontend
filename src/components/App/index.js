0e57b46a7eb4ebd436132339d959bab56d905bc1
Meike Pollakowski <meike.pollakowski@gmail.com>
2018-10-06 16:40:13 +0200

    Proposal form

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,19 +1,34 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Form from '../Form'
-import List from '../List'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Form from "../Form";
+import List from "../List";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import purple from "@material-ui/core/colors/purple";
+import green from "@material-ui/core/colors/green";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: green
+  },
+  status: {
+    danger: "orange"
+  }
+});
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Route exact path="/" component={List} />
-          <Route path="/form" component={Form} />
-        </div>
-      </Router>
-    )
+      <MuiThemeProvider theme={theme}>
+        <Router>
+          <div>
+            <Route exact path="/" component={List} />
+            <Route path="/form" component={Form} />
+          </div>
+        </Router>
+      </MuiThemeProvider>
+    );
   }
 }
 
-export default App
+export default App;
