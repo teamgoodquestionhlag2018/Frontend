6c2ccacf6d0cda6bf3eb00e49d870a2cadb369c7
imeugenia <jevgenijazigisova@gmail.com>
2018-10-07 14:40:56 +0200

    Web3js

--- src/components/Proposal/index.js
+++ src/components/Proposal/index.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
-import axios from "axios";
-import Typography from "@material-ui/core/Typography";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
+import React, { Component } from 'react'
+import axios from 'axios'
+import Typography from '@material-ui/core/Typography'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Button from '@material-ui/core/Button'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import { createMilestone, setDone } from '../../contract'
 
 class Proposal extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       proposal: undefined
-    };
+    }
   }
   // componentDidMount() {
   //   axios
@@ -40,98 +40,103 @@ class Proposal extends Component {
         `http://localhost:3001/proposals/${this.props.match.params.id}`,
         {
           headers: {
-            token: "4cfe8299-ccdb-4483-9ec1-0f4ddcb08743"
+            token: '4cfe8299-ccdb-4483-9ec1-0f4ddcb08743'
           }
         }
-      );
-      console.log(proposal);
+      )
+      console.log(proposal)
       this.setState({ proposal: proposal.data }, () => {
-        console.log("Current state: ", this.state);
-      });
+        console.log('Current state: ', this.state)
+      })
     } catch (error) {
-      console.log("Error", error);
+      console.log('Error', error)
     }
   }
 
-  handleClick = async (person, id) => {
-    if (person === "client") {
+  handleClick = async (person, id, milestone) => {
+    if (person === 'client') {
       try {
+        await createMilestone(milestone, true)
+        console.log('Signed', 'Client:', true)
+
         const proposalNew = await axios.put(
           `http://localhost:3001/milestones/${id}/client/sign`,
           { signed: true },
           {
             headers: {
-              token: "4cfe8299-ccdb-4483-9ec1-0f4ddcb08743"
+              token: '4cfe8299-ccdb-4483-9ec1-0f4ddcb08743'
             }
           }
-        );
+        )
         this.setState({
           proposal: proposalNew.data
-        });
+        })
         // this.setState({ signed: true });
       } catch (error) {
-        console.log("Error in client signature", error);
+        console.log('Error in client signature', error)
       }
-    } else if (person === "freelancer") {
+    } else if (person === 'freelancer') {
       try {
+        await createMilestone(milestone, false)
+        console.log('Signed', 'Client:', false)
         const proposalNew = await axios.put(
           `http://localhost:3001/milestones/${id}/freelancer/sign`,
           true,
           {
             headers: {
-              token: "4cfe8299-ccdb-4483-9ec1-0f4ddcb08743"
+              token: '4cfe8299-ccdb-4483-9ec1-0f4ddcb08743'
             }
           }
-        );
-        this.setState({ proposal: proposalNew.data });
+        )
+        this.setState({ proposal: proposalNew.data })
         // this.setState({ signed: true });
       } catch (error) {
-        console.log("Error in freelancer signature", error);
+        console.log('Error in freelancer signature', error)
       }
     }
-  };
+  }
 
   render() {
     // const proposal = this.state.proposal ? this.state.proposal : null;
     // console.log(proposal);
     // return proposal ? (
     if (!this.state.proposal) {
-      return null;
+      return null
     } else {
-      const proposal = this.state.proposal ? this.state.proposal : null;
+      const proposal = this.state.proposal ? this.state.proposal : null
       return (
-        <div style={{ padding: "4rem" }}>
-          <Card style={{ padding: "3em", maxWidth: "80%" }}>
-            <CardContent style={{ marginBottom: "4em" }}>
+        <div style={{ padding: '4rem' }}>
+          <Card style={{ padding: '3em', maxWidth: '80%' }}>
+            <CardContent style={{ marginBottom: '4em' }}>
               <Typography color="textSecondary">Proposal:</Typography>
               <Typography
                 variant="headline"
                 component="h1"
-                style={{ marginBottom: "2em" }}
+                style={{ marginBottom: '2em' }}
               >
                 {proposal.title}
               </Typography>
               <Typography variant="body1" component="p">
-                This Freelance Contract is entered into and made effective as of{" "}
+                This Freelance Contract is entered into and made effective as of{' '}
                 {
                   // Format date
                 }
-                {proposal.creationDate}, by and between the client{" "}
-                {proposal.client.name} ("Client") and the contractor{" "}
+                {proposal.creationDate}, by and between the client{' '}
+                {proposal.client.name} ("Client") and the contractor{' '}
                 {proposal.freelancer.name} (“Freelancer”).
               </Typography>
               <Typography variant="body1" component="p">
-                {" "}
+                {' '}
                 Both parties agree as follows:
               </Typography>
-              <Typography variant="subheading" style={{ marginTop: "10px" }}>
+              <Typography variant="subheading" style={{ marginTop: '10px' }}>
                 Description of the Services:
               </Typography>
               <Typography variant="subheading">{proposal.summary}</Typography>
               <Typography
                 variant="title"
                 component="h2"
-                style={{ margin: "2em 0 1em" }}
+                style={{ margin: '2em 0 1em' }}
               >
                 Milestones
               </Typography>
@@ -161,40 +166,52 @@ class Proposal extends Component {
                         />
                       </ListItem>
                       {milestone.client.signed ? (
-                        <Typography style={{ color: "green" }}>
+                        <Typography style={{ color: 'green' }}>
                           {milestone.client} (client) signed
                         </Typography>
                       ) : (
                         <Button
                           variant="contained"
                           onClick={() =>
-                            this.handleClick("client", milestone.id)
+                            this.handleClick('client', milestone.id, milestone)
                           }
                         >
                           Client: sign milestone
                         </Button>
                       )}
                       {milestone.freelancer.signed ? (
-                        <Typography style={{ color: "green" }}>
+                        <Typography style={{ color: 'green' }}>
                           {milestone.freelancer} (freelancer) signed
                         </Typography>
                       ) : (
                         <Button
                           variant="contained"
                           onClick={() =>
-                            this.handleClick("freelancer", milestone.id)
+                            this.handleClick(
+                              'freelancer',
+                              milestone.id,
+                              milestone
+                            )
                           }
                         >
                           Freelancer: sign milestone
                         </Button>
                       )}
+                      <Button
+                        variant="outlined"
+                        onClick={() => {
+                          setDone(milestone.id)
+                        }}
+                      >
+                        Done
+                      </Button>
                     </List>
-                  );
+                  )
                 })}
               <Typography
                 variant="title"
                 component="h2"
-                style={{ margin: "2em 0 1em" }}
+                style={{ margin: '2em 0 1em' }}
               >
                 Payment Terms/Schedule
               </Typography>
@@ -209,7 +226,7 @@ class Proposal extends Component {
               <Typography
                 variant="title"
                 component="h2"
-                style={{ margin: "2em 0 1em" }}
+                style={{ margin: '2em 0 1em' }}
               >
                 Terms and Conditions
               </Typography>
@@ -220,10 +237,10 @@ class Proposal extends Component {
             </CardContent>
           </Card>
         </div>
-      );
+      )
     }
     // ) : null;
   }
 }
 
-export default Proposal;
+export default Proposal
