1801434f57db7fe9c3d52ef6805d5d32726d0972
Heini Ahokas <ahokas.heini@gmail.com>
2018-10-07 12:07:55 +0200

    add milestone checkbox icon for list

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -7,71 +7,114 @@ import Divider from "@material-ui/core/Divider";
 import Typography from "@material-ui/core/Typography";
 import { Link } from "react-router-dom";
 import AddIcon from "@material-ui/icons/Add";
-import Button from "@material-ui/core/Button";
+import CheckBox from "@material-ui/icons/CheckBox";
+import IndeterminateCheckBox from "@material-ui/icons/IndeterminateCheckBox";
 import Tooltip from "@material-ui/core/Tooltip";
 
 class ProposalList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  componentDidMount() {
-    axios.get("http://localhost:3001/proposals").then(({ data }) => {
-      this.setState({ data });
-      console.log(data);
-    });
-  }
-  render() {
-    return (
-      <List style={{ width: "700px" }}>
-        <Typography
-          variant="headline"
-          style={{
-            float: "left"
-          }}
-        >
-          Your Contracts
-        </Typography>
-        <div
-          style={{
-            float: "right",
-            marginRight: "20px"
-          }}
-        >
-          <Tooltip title="Create new contract">
-            <Link to={"/form"}>
-              <Button variant="fab" color="secondary">
-                <AddIcon />
-              </Button>
-            </Link>
-          </Tooltip>
-        </div>
-        {this.state.data &&
-          this.state.data.map(contract => {
-            var dateNumber = parseInt(contract.creationDate);
-            return (
-              <Link
-                to={`/proposals/${contract.id}`}
-                key={contract.id}
-                style={{
-                  textDecoration: "none"
-                }}
-              >
-                <div>
-                  <ListItem button>
-                    <ListItemText
-                      primary={contract.title || "Contract Name"}
-                      secondary={new Date(dateNumber).toLocaleDateString()}
-                    />
-                  </ListItem>
-                  <Divider />
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    componentDidMount() {
+        axios.get("http://localhost:3001/proposals").then(({ data }) => {
+            this.setState({ data });
+            console.log(data);
+        });
+    }
+    render() {
+        return (
+            <List style={{ width: "700px", marginLeft: "20px" }}>
+                <Typography
+                    variant="headline"
+                    style={{
+                        float: "left",
+                        paddingBottom: "20px"
+                    }}
+                >
+                    Your Contract Proposals
+                </Typography>
+                <div
+                    style={{
+                        float: "right",
+                        marginRight: "20px"
+                    }}
+                >
+                    <Tooltip title="Create new contract proposal">
+                        <Link to={"/form"}>
+                            <AddIcon />
+                        </Link>
+                    </Tooltip>
                 </div>
-              </Link>
-            );
-          })}
-      </List>
-    );
-  }
+                {this.state.data &&
+                    this.state.data.map(contract => {
+                        var dateNumber = parseInt(contract.creationDate);
+                        console.log("milestones " + contract.milestones);
+                        return (
+                            <Link
+                                to={`/proposals/${contract.id}`}
+                                key={contract.id}
+                                style={{
+                                    textDecoration: "none"
+                                }}
+                            >
+                                <div>
+                                    <ListItem button>
+                                        <ListItemText
+                                            primary={
+                                                contract.contractTitle ||
+                                                "Contract Proposal Name"
+                                            }
+                                            secondary={new Date(
+                                                dateNumber
+                                            ).toLocaleDateString()}
+                                        />
+                                        {contract.milestones.length &&
+                                            contract.milestones.map(
+                                                milestone => {
+                                                    return (
+                                                        <Tooltip
+                                                            title={
+                                                                milestone.client
+                                                                    .signed &&
+                                                                milestone
+                                                                    .freelancer
+                                                                    .signed
+                                                                    ? "signed"
+                                                                    : "not signed"
+                                                            }
+                                                        >
+                                                            {milestone.client
+                                                                .signed &&
+                                                            milestone.freelancer
+                                                                .signed ? (
+                                                                <CheckBox
+                                                                    style={{
+                                                                        color:
+                                                                            "green"
+                                                                    }}
+                                                                />
+                                                            ) : (
+                                                                <IndeterminateCheckBox
+                                                                    style={{
+                                                                        color:
+                                                                            "grey"
+                                                                    }}
+                                                                />
+                                                            )}
+                                                        </Tooltip>
+                                                    );
+                                                }
+                                            )}
+                                    </ListItem>
+                                    <Divider />
+                                </div>
+                            </Link>
+                        );
+                    })}
+            </List>
+        );
+    }
 }
 
 export default ProposalList;
